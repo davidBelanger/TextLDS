af6ba09b3e9e07fc0ebce7c65c86cce7f9650585
David Belanger <david.b.belanger@gmail.com>
2017-04-27 16:45:50 -0400

    matlab/ASOS/eextract.m

---
+++ matlab/ASOS/eextract.m
@@ -0,0 +1,10 @@
+function [Ex_x_0, Ex_x_1, Ey_x_0, Eu_x_0, Eu_x_1, Ex_, Exx_, x1_] = eextract( expt )
+    Ex_x_0 = expt.Ex_x_0;
+    Ex_x_1 = expt.Ex_x_1;
+    Ey_x_0 = expt.Ey_x_0;
+    Eu_x_0 = expt.Eu_x_0;
+    Eu_x_1 = expt.Eu_x_1;
+    Ex_ = expt.Ex_;
+    Exx_ = expt.Exx_;
+    x1_ = expt.x1_;
+end
