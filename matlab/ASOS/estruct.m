b3dfbbd940724c6cfa6c65d73849e8a85e8fbaa3
David Belanger <david.b.belanger@gmail.com>
2017-04-27 15:59:39 -0400

    estruct

---
+++ matlab/ASOS/estruct.m
@@ -0,0 +1,10 @@
+function expt = estruct( Ex_x_0, Ex_x_1, Ey_x_0, Eu_x_0, Eu_x_1, Ex_, Exx_, x1_ )
+    expt.Ex_x_0 = Ex_x_0;
+    expt.Ex_x_1 = Ex_x_1;
+    expt.Ey_x_0 = Ey_x_0;
+    expt.Eu_x_0 = Eu_x_0;
+    expt.Eu_x_1 = Eu_x_1;
+    expt.Ex_ = Ex_;
+    expt.Exx_ = Exx_;
+    expt.x1_ = x1_;
+end
